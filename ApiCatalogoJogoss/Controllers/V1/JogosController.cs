bf82e427862b0e2a35578575b69b6bd839e75663
Vinicius Parente <vd.parente@hotmail.com>
2021-07-16 20:42:42 -0300

    Part2

---
+++ ApiCatalogoJogoss/Controllers/V1/JogosController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogoss.Controllers.V1
+{
+    [Route("api/V1/[controller]")]
+    [ApiController]
+    public class JogosController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<object>>> Obter()
+        {
+            return Ok();
+        }
+
+        [HttpGet ("{idJogo: guid}")]
+        public async Task<ActionResult<object>> Obter(Guid idJogo)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<object>> InserirJogo()
+        {
+            return Ok();
+        }
+
+        [HttpPut("{idJogo:guid}")]
+        public async Task<ActionResult> AtualizarJogo(Guid idJogo, object jogo)
+        {
+            return Ok();
+        }
+
+        [HttpPatch("{idJogo:guid}/preco/{preco:double}")]
+        public async Task<ActionResult> AtualizarJogo(Guid idJogo, double preco)
+        {
+            return Ok();
+        }
+
+
+        [HttpDelete("{idJogo:guid}")]
+        public async Task<ActionResult<object>> ApagarJogo(Guid idJogo)
+        {
+            return Ok();
+        }
+
+
+    }
+}
