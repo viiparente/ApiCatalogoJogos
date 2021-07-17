f26534664c3ccb8355c551d6dba5a422c51bcdb9
Vinicius Parente <vd.parente@hotmail.com>
2021-07-16 21:41:11 -0300

    Part5

--- ApiCatalogoJogoss/Startup.cs
+++ ApiCatalogoJogoss/Startup.cs
@@ -1,3 +1,6 @@
+using ApiCatalogoJogoss.Repositories;
+using ApiCatalogoJogoss.Services;
+using ExemploApiCatalogoJogos.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +29,8 @@ namespace ApiCatalogoJogoss
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IJogoService, JogoService>();
+            services.AddScoped<IJogoRepository, JogoRepository>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
